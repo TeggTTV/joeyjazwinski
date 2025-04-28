e89391762b79fb7397c1aa1d0ae1c49ef2ebc927
Joey Jazwinski <doe034945@gmail.com>
2025-04-28 00:31:02 -0400

    asd

--- src/components/MaxWidthWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "../lib/utils";
-
-export default function MaxWidthWrapper({ children, className }: { children: React.ReactNode, className?: string }) {
-    return (
-        <div className={cn("mt-10 max-w-4xl w-full dark:bg-gray-900 left-0 right-0 mx-auto", className)}>
-            {children}
-        </div>
-    );
-}
