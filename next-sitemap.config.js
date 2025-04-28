d0f371a820051df951151f8cf64001c1afe4bdbb
TeggTTV <doe034945@gmail.com>
2025-04-28 09:20:02 -0400

    next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://myfullname.vercel.app',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://joeyjazwinski.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 5000,
 };
