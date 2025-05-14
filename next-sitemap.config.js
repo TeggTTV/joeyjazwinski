b1b64349d67b85698dd3cdee5f249f001fd00ca3
Joey Jazwinski <doe034945@gmail.com>
2025-05-14 16:28:40 -0400

    asd

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,17 @@
 module.exports = {
 	siteUrl:
 		process.env.NEXT_PUBLIC_VERCEL_ENV || 'https://joeyjazwinski.vercel.app',
-	generateRobotsTxt: true,
-	sitemapSize: 5000,	
-	additionalSitemaps: ['https://joeyjazwinski.vercel.app/server-sitemap.xml'],
+		sitemapSize: 5000,	
+		additionalSitemaps: ['https://joeyjazwinski.vercel.app/server-sitemap.xml'],
+
+		generateRobotsTxt: true,
+		robotsTxtOptions: {
+			additionalSitemaps: [
+				'https://joeyjazwinski.vercel.app/server-sitemap.xml',
+			],
+			UserAgent: '*',
+			Host: 'https://joeyjazwinski.vercel.app',
+			Disallow: '/api/',
+			Allow: '/',
+		},
 };
