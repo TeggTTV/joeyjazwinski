21aa7eac93eeda4b4b17a9effd8d91ccea769083
TeggTTV <doe034945@gmail.com>
2025-05-16 18:58:50 -0400

    asd

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,16 +6,16 @@ module.exports = {
 	sitemapSize: 5000,
 	additionalSitemaps: ['https://joeyjazwinski.vercel.app/server-sitemap.xml'],
 
-	generateRobotsTxt: true,
-	robotsTxtOptions: {
-		additionalSitemaps: [
-			'https://joeyjazwinski.vercel.app/server-sitemap.xml',
-			'https://joeyjazwinski.vercel.app/sitemap.xml',
-			'https://joeyjazwinski.vercel.app/sitemap-0.xml',
-		],
-		UserAgent: '*',
-		Host: 'https://joeyjazwinski.vercel.app',
-		Disallow: '/api/',
-		Allow: '/',
-	},
+	// generateRobotsTxt: true,
+	// robotsTxtOptions: {
+	// 	additionalSitemaps: [
+	// 		'https://joeyjazwinski.vercel.app/server-sitemap.xml',
+	// 		'https://joeyjazwinski.vercel.app/sitemap.xml',
+	// 		'https://joeyjazwinski.vercel.app/sitemap-0.xml',
+	// 	],
+	// 	UserAgent: '*',
+	// 	Host: 'https://joeyjazwinski.vercel.app',
+	// 	Disallow: '/api/',
+	// 	Allow: '/',
+	// },
 };
